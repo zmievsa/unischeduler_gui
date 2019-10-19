42e31f18021638961b09083c0efb64fd05cc7e5e
Varabe <zertarx@gmail.com>
2019-10-19 16:38:17 -0400

    Finished graphical interface

--- GUI.py
+++ GUI.py
@@ -1,41 +1,37 @@
 import tkinter as tk
 import schedule
+import traceback
 
 
 class SchedulerGui:
     def __init__(self, master):
         self.master = master
         master.title("Scheduler")
-
-        self.label = tk.Label(master, text="Please, fill out the fields below")
+        self.label_text = tk.StringVar()
+        self.label_text.set("Please, paste your schedule in the field below")
+        self.label = tk.Label(master, textvariable=self.label_text)
         self.label.pack()
-
-        self.year_label = tk.Label(master, text="Year:")
-        self.year_label.pack()
-        self.year_entry = tk.Entry(master, width=10)
-        self.year_entry.pack()
-
-        self.term_label = tk.Label(master, text="Term:")
-        self.term_label.pack()
-        self.term_list = tk.Listbox(master, selectmode=tk.SINGLE)
-        self.term_list.insert(tk.END, "Fall")
-        self.term_list.insert(tk.END, "Spring")
-        self.term_list.config(width=0, height=0)
-        self.term_list.pack()
-        self.term_list.select_anchor(0)
-        self.term_list.select_set(0)
-        self.term_list.activate(0)
-
-        self.schedule_entry = tk.Text(master, height=3)
+        self.schedule_entry = tk.Text(master, height=9)
         self.schedule_entry.pack()
 
         self.enter_button = tk.Button(master, text="Enter", command=self.create_schedule)
         self.enter_button.pack()
 
     def create_schedule(self):
-        year: str = self.year_entry.get()
-        if year.isdigit():
-            schedule.main(int(year), self.term_list.get(self.term_list.curselection()), self.schedule_entry.get("1.0", tk.END))
+        self.enter_button.pack_forget()
+        self.label_text.set('Starting...')
+        self.master.update()
+        try:
+            schedule.main(self.schedule_entry.get("1.0", tk.END))
+        except Exception as e:
+            with open('log', 'a') as f:
+                self.label_text.set('ERROR OCCURRED. CHECK LOG FILE')
+                f.write(str(e))
+                f.write(traceback.format_exc())
+        else:
+            self.label_text.set('Finished!')
+            self.enter_button.pack()
+            
 
 
 if __name__ == "__main__":
