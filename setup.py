9595266e45775cff22a77bdbd435c326345b42af
Stanislav Zmiev <zertarx@gmail.com>
2019-11-03 17:37:22 -0500

    [change] Use pyinstaller instead of py2app

--- setup.py
+++ setup.py
@@ -1,39 +1,27 @@
 import shutil
 import sys
 from pathlib import Path
-from unittest.mock import patch
 
-setup_requires = []
+import PyInstaller.__main__
 
 curpath = Path(__file__).parent
 NAME = "Scheduler"
 APP_PATH = curpath / "src/GUI_QT.py"
 APP = [str(APP_PATH)]
+
 if sys.platform.startswith("win32"):
-    import PyInstaller.__main__
     extension = "exe"
     final_dir = "Windows"
-    new_app_path = Path(curpath / "src/GUI.pyw")
-    APP_PATH.rename(new_app_path)
-    PyInstaller.__main__.run(
-        ["-n", NAME, "--onefile", "--clean", str(new_app_path)])
-    new_app_path.rename(APP_PATH)
-    Path(curpath / f"{NAME}.spec").unlink()
 elif sys.platform.startswith("darwin"):
-    from setuptools import setup
     extension = "app"
     final_dir = "OS X"
-    with patch('sys.argv', ["setup.py", "py2app"]):
-        setup(
-            name=NAME,
-            app=APP,
-            data_files=[],
-            options={"py2app": {}},
-            setup_requires=setup_requires
-        )
 else:
     #  No building for linux
     exit(0)
+
+ui_data = "src/qtgui.ui;."
+PyInstaller.__main__.run(
+    ["--onefile", "--noconsole", "--clean", "--name=" + NAME, "--add-data=" + ui_data, str(APP_PATH)])
 
 file_name = f"{NAME}.{extension}"
 release_folder = Path(curpath / f"release/{final_dir}")
