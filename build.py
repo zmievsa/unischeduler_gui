005864ca7bb90efe46d25610f2affbf54bc5d12f
Stanislav Zmiev <zertarx@gmail.com>
2019-11-04 02:12:55 -0500

    [ref] Clean up build.py

--- build.py
+++ build.py
@@ -8,14 +8,11 @@ import PyInstaller.__main__
 curpath = Path(__file__).parent
 NAME = "Scheduler"
 APP_PATH = curpath / "src/GUI_QT.py"
-APP = [str(APP_PATH)]
 
 if sys.platform.startswith("win32"):
-    extension = "exe"
     final_dir = "Windows"
     delimeter = ";"
 elif sys.platform.startswith("darwin"):
-    extension = "app"
     final_dir = "OS X"
     delimeter = ":"
 else:
@@ -24,11 +21,9 @@ else:
 
 
 PyInstaller.__main__.run(
-    ["--onefile", "--noconsole", "--clean", "--name=" + NAME, f"--add-data={DATA_FOLDER}{delimeter}data", str(APP_PATH)])
-
-file_name = f"{NAME}.{extension}"
-release_folder = Path(curpath / f"release/{final_dir}")
-release_folder.mkdir(parents=True, exist_ok=True)
-Path(curpath / f"dist/{file_name}").replace(release_folder / file_name)
-shutil.rmtree(curpath / "build")
-shutil.rmtree(curpath / "dist")
+    [
+        str(APP_PATH), "--onefile", "--noconsole", "--clean",
+        f"--name={NAME}", f"--distpath={curpath}/release/{final_dir}",
+        f"--add-data={DATA_FOLDER}{delimeter}data",
+    ]
+)
